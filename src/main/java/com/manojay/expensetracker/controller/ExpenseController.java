819526b170e7f259b747c3841075a141e90fe610
Manojay13 <electraatoma@gmail.com>
2025-05-28 20:08:29 +0530

    Added @CrossOrrigin annotation

--- src/main/java/com/manojay/expensetracker/controller/ExpenseController.java
+++ src/main/java/com/manojay/expensetracker/controller/ExpenseController.java
@@ -3,6 +3,7 @@ package com.manojay.expensetracker.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.manojay.expensetracker.model.Expense;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/expenses")
 public class ExpenseController {
